52324b71afd1963595893e7e84809270460e9b42
Sam Fish <samuel.fish@live.com>
2022-01-09 10:54:10 -0800

    Added progressive web app capability

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
   images: {
     domains: ["webapp-profile-images.s3.amazonaws.com"],
   },
-};
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    // disable: process.env.NODE_ENV === "development",
+  }
+});
