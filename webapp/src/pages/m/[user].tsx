27c26e29ce57d63571219bfc33a7e50e276a991b
Sam Fish <samuel.fish@live.com>
2022-02-17 21:29:20 -0800

    Fixed profile editing not working for logged in user

--- webapp/src/pages/m/[user].tsx
+++ webapp/src/pages/m/[user].tsx
@@ -64,8 +64,7 @@ export const Profile: NextPage = () => {
         </title>
       </Head>
       <NavBar />
-      {(!user?.username && !userFetching) ||
-      (!user?.profileSetup && !isMyProfile) ? (
+      {!user?.profileSetup && !isMyProfile && !userFetching ? (
         <Box bg={"dark.25"} minH="100vh" pt={"20"} px={5}>
           <Card maxW="4xl" mx="auto" textAlign="center">
             <Heading p={5} fontSize={{ base: "3xl", md: "4xl" }}>
@@ -84,7 +83,7 @@ export const Profile: NextPage = () => {
             setTabIndex={setTabIndex}
           />
 
-          {user && isMyProfile && !user.profileSetup && (
+          {user && isMyProfile && (
             <UserProfileSetup user={user} isOpen={isOpen} onClose={onClose} />
           )}
 
